09e6b81297bc5386a50c4bdcfde89b215398755c
dhanzhang <d.h.z@live.cn>
2012-09-14 16:24:06 +0800

    Init

---
+++ Pjm.Data/BankAccount.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pjm.Data
+{
+    /// <summary> 
+    /// Bank Account demo class. 
+    /// </summary> 
+    public class BankAccount
+    {
+        private string m_customerName;
+
+        private double m_balance;
+
+        private bool m_frozen = false;
+
+        private BankAccount()
+        {
+        }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        public void Debit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+
+        public void Credit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+
+        private void FreezeAccount()
+        {
+            m_frozen = true;
+        }
+
+        private void UnfreezeAccount()
+        {
+            m_frozen = false;
+        } 
+    }
+}
